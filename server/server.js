63639bc9fd055c12302072e23f4246bcdd54342b
Chad Kirk <ChadWKirk@gmail.com>
2023-08-09 22:30:34 -0500

    Connected AWS RDS Aurora MySQL to server

--- server/server.js
+++ server/server.js
@@ -1,7 +1,24 @@
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 5000;
+require("dotenv").config({ path: "./config.env" });
 const cors = require("cors");
+const mysql = require("mysql");
+const db = mysql.createConnection({
+  host: "the-void.cluster-coxkoizjkqbn.us-east-2.rds.amazonaws.com",
+  port: "3306",
+  user: process.env.AWS_USER,
+  password: process.env.AWS_PASSWORD,
+  database: "thevoid",
+});
+
+db.connect((err) => {
+  if (err) {
+    console.log(err.message);
+    return;
+  }
+  console.log("Database connected.");
+});
 
 app.use(cors());
 
