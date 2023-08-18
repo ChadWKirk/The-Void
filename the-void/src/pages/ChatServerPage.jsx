f3e930fafe479c919795b4d0612d483114353444
Chad Kirk <ChadWKirk@gmail.com>
2023-08-17 20:12:51 -0500

    Added get request for entering chat room

--- the-void/src/pages/ChatServerPage.jsx
+++ the-void/src/pages/ChatServerPage.jsx
@@ -1,6 +1,18 @@
 import { React, useEffect, useState } from "react";
 
 const ChatServerPage = () => {
+  useEffect(() => {
+    async function AddName() {
+      await fetch("/api/test", {
+        METHOD: "GET",
+        headers: { "Content-type": "Application/json" },
+        body: "TestName",
+      }).then((response) => {
+        console.log(response);
+      });
+    }
+    AddName();
+  }, []);
   return (
     <div className="container">
       <input placeholder="Message" className="nameInput" maxLength="16"></input>
